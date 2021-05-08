4dbe9a6cc9443fcc59dbc061821a574fa4455d7e
sunny-prakash <sunny.pprakash@gmail.com>
2021-05-08 18:09:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
-function App() {
-  return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
-  );
-}
+const App = () => {
+    const [showpara, showParagraph] = useState(false);
+
+    const onClicking = () => {
+        showParagraph(true);
+    };
+    return (
+        <div id="main">
+            {/* Do not remove this main div!! */}
+            <button id="click" onClick={() => onClicking()}>
+                {"Click me"}
+            </button>
+            {showpara ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : ""}
+        </div>
+    );
+};
 
 
 export default App;
